fc52e03b2995723a1d745240ca6f71080f44b6c9
perlmathi <perlmathi@gmail.com>
2024-11-03 14:58:16 +0530

    changef main.cpp

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ float div(int a, int b);
 int main()
 {
 cout<<"welocme to c++"<<endl;
-cour<<"sum:"<<sum(2,3)<<endl;
-cour<<"div:"<<div(4,3)<<endl;
+cout<<"sum:"<<sum(2,3)<<endl;
+cout<<"div:"<<div(4,3)<<endl;
 
 
 }
